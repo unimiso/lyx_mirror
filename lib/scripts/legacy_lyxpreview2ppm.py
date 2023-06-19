c5c03f9be392b0447ceb4d5c3272d07790578380
Enrico Forestieri <forenr@lyx.org>
2023-06-19 18:30:23 +0200

    Amend b135c965
    A macro with @ in its name must be used after \makeatletter

--- lib/scripts/legacy_lyxpreview2ppm.py
+++ lib/scripts/legacy_lyxpreview2ppm.py
@@ -253,9 +253,9 @@ def legacy_latex_file(latex_file, fg_color, bg_color):
 \\pagecolor{lyxbg}
 \\usepackage[%s,tightpage]{preview}
 \\usepackage{ifthen}
-\\ifthenelse{\equal{\f@family}{cmr}}{
-\\IfFileExists{lmodern.sty}{\\usepackage{lmodern}}{\\usepackage{ae,aecompl}}}{}
 \\makeatletter
+\\ifthenelse{\\equal{\\f@family}{cmr}}{
+\\IfFileExists{lmodern.sty}{\\usepackage{lmodern}}{\\usepackage{ae,aecompl}}}{}
 \\g@addto@macro\\preview{\\begingroup\\color{lyxbg}\\special{ps::clippath fill}\\color{lyxfg}}
 \\g@addto@macro\\endpreview{\\endgroup}
 \\makeatother
